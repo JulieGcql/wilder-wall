1c85c24098dfc045b418647cd570f825bbfc33e9
David <eth3rnit3@outlook.fr>
2018-11-15 18:23:28 +0100

    start widers wall

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import {Switch, Route} from 'react-router-dom'
 import './App.css';
+import Wall from "./components/wall"
 import NotFound from './components/NotFound';
 import JohnDoe from './components/wilders/JohnDoe/JohnDoe';
 import MaximePochet from './components/wilders/MaximePochet/MaximePochet';
@@ -10,10 +11,9 @@ class App extends Component {
   render() {
     return (
       <Switch>
+        <Route exact path="/wilders/teams" component={Wall} />
         <Route exact path="/wilders/john-doe" component={JohnDoe} />
         <Route exact path="/wilders/maxime-pochet" component={MaximePochet} />
-
-
         <Route exact path="/wilders/julie-lucanik" component={JulieLucanik} />
         <Route path="/" component={NotFound} />
       </Switch>
