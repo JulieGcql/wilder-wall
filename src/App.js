f619751bf4cb47240a3c81afcc9ecca7e42ea0c7
Nuit Blanche <aloys.grimaldi@gmail.com>
2018-11-15 12:07:09 +0100

    création branche

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import React, { Component } from 'react';
 import {Switch, Route} from 'react-router-dom'
 import './App.css';
 import NotFound from './components/NotFound';
-import JohnDoe from './components/wilders/JohnDoe/JohnDoe';
+import AloysGrimaldi from './components/wilders/AloysGrimaldi/AloysGrimaldi';
 
 class App extends Component {
   render() {
     return (
       <Switch>
-        <Route exact path="/wilders/john-doe" component={JohnDoe} />
+        <Route exact path="/wilders/aloys-grimaldi" component={AloysGrimaldi} />
         <Route path="/" component={NotFound} />
       </Switch>
     );
---
+++ src/components/wilders/AloysGrimaldi/AloysGrimaldi.jsx
@@ -0,0 +1,41 @@
+import React, { Component } from 'react'
+import './AloysGrimaldi.scss'
+import Spinner from '../../Spinner';
+const BASE_URL = "https://peaceful-cliffs-33252.herokuapp.com/api/v1/wilders"
+const USERNAME = "aloys-grimaldi"
+
+export default class AloysGrimaldi extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      user: {},
+      userFetched: false
+    }
+  }
+
+  componentDidMount(){
+    fetch(`${BASE_URL}/${USERNAME}`)
+    .then((response) => response.json())
+    .then((data) => this.setState({user: data.user, userFetched: true}))
+    .catch((error) => console.log(error))
+  }
+  
+  render() {
+    const {user, userFetched} = this.state;
+    if(!userFetched){
+      return <div className="container spinnerContainer"><Spinner/></div>
+    }
+    return (
+      <div className="cvContainer">
+        <img src={user.photo} />
+        <h1>{`${user.firstname} ${user.lastname}`}</h1>
+        <div dangerouslySetInnerHTML={createMarkup(user.bio)}></div>
+        <a href={user.cv}>Télécharger mon CV !</a>
+      </div>
+    )
+  }
+}
+
+function createMarkup(stringyfiedHtml) {
+  return {__html: stringyfiedHtml};
+}
