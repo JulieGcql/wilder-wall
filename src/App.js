e25f12f76fc252764a0a7bfaa51489665c500ce0
lydia.dal@live.fr <lydia.dal@live.fr>
2018-11-15 17:58:51 +0100

    Cv lydia

--- src/App.js
+++ src/App.js
@@ -3,13 +3,16 @@ import {Switch, Route} from 'react-router-dom'
 import './App.css';
 import NotFound from './components/NotFound';
 import JohnDoe from './components/wilders/JohnDoe/JohnDoe';
+import LydiaDali from './components/wilders/LydiaDali/LydiaDali';
 
 class App extends Component {
   render() {
     return (
       <Switch>
         <Route exact path="/wilders/john-doe" component={JohnDoe} />
+        <Route exact path="/wilders/lydia-dali" component={LydiaDali} />
         <Route path="/" component={NotFound} />
+        
       </Switch>
     );
   }
