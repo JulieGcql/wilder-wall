d6f14da9b9b479aced1a9e57c8246787b80f5f87
Reb Sen <archivesreb@gmail.com>
2018-11-16 14:27:10 +0100

    debut de suivit de la racine du repertoire, apres avoir tracke depuis le rep semantic

--- src/components/wilders/RebeccaSenechal/RebeccaSenechal.jsx
+++ src/components/wilders/RebeccaSenechal/RebeccaSenechal.jsx
@@ -1,11 +1,10 @@
 import React, { Component } from 'react'
-import './JohnDoe.scss'
 import Spinner from '../../Spinner';
 const BASE_URL = "https://peaceful-cliffs-33252.herokuapp.com/api/v1/wilders"
-const USERNAME = "senechal-rebecca"
+const USERNAME = "rebecca-senechal"
+//import 'RebeccaSenechal.scss'
 
-
-export default class JohnDoe extends Component {
+export default class RebeccaSenechal extends Component {
   constructor(props){
     super(props);
     this.state = {
@@ -19,24 +18,31 @@ export default class JohnDoe extends Component {
     .then((response) => response.json())
     .then((data) => this.setState({user: data.user, userFetched: true}))
     .catch((error) => console.log(error))
+    console.log("user1 :", this.state.user)
+
+    
   }
   
+ 
+ 
   render() {
+    console.log("user :", this.state.user)
     const {user, userFetched} = this.state;
     if(!userFetched){
       return <div className="container spinnerContainer"><Spinner/></div>
     }
     return (
       <div className="cvContainer">
-        <img src={user.photo} alt="visage"/>
+        <img src={user.photo} alt="visage" />
         <h1>{`${user.firstname} ${user.lastname}`}</h1>
         <div dangerouslySetInnerHTML={createMarkup(user.bio)}></div>
         <a href={user.cv}>Télécharger mon CV !</a>
       </div>
     )
   }
+ 
+ 
 }
-
 function createMarkup(stringyfiedHtml) {
   return {__html: stringyfiedHtml};
 }
