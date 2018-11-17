3dcfc4911a79d7fb1f7482b0fbc7822ca902aa8d
Chris Chatroux <icecriss@hotmail.com>
2018-11-18 00:04:03 +0100

    React Resume done

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
-import React, { Component } from 'react';
-import {Switch, Route} from 'react-router-dom'
-import './App.css';
-import NotFound from './components/NotFound';
-import JohnDoe from './components/wilders/JohnDoe/JohnDoe';
+import React, { Component } from "react";
+import { Switch, Route } from "react-router-dom";
+// import "./App.css";
+import NotFound from "./components/NotFound";
+import JohnDoe from "./components/wilders/JohnDoe/JohnDoe";
+import ChristelleChatroux from "./components/wilders/ChristelleChatroux/ChristelleChatroux";
 
 class App extends Component {
   render() {
     return (
       <Switch>
         <Route exact path="/wilders/john-doe" component={JohnDoe} />
+        <Route
+          exact
+          path="/wilders/christelle-chatroux"
+          component={ChristelleChatroux}
+        />
         <Route path="/" component={NotFound} />
       </Switch>
     );
