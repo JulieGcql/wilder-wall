9ce139bb721532bbbf5b11fba70a42e955fc7139
Julien <julien.bertocchini@gmail.com>
2018-11-18 23:23:24 +0100

    Fontionnal application

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import {Switch, Route} from 'react-router-dom'
 import './App.css';
 import NotFound from './components/NotFound';
 import JohnDoe from './components/wilders/JohnDoe/JohnDoe';
+import JulienBertocchini from './components/wilders/JulienBertocchini/JulienBertocchini';
 
 class App extends Component {
   render() {
     return (
       <Switch>
-        <Route exact path="/wilders/john-doe" component={JohnDoe} />
-        <Route path="/" component={NotFound} />
+        <Route path="/john-doe" component={JohnDoe} />
+        <Route exact path="/wilders/julien-bertocchini" component={JulienBertocchini} />
       </Switch>
     );
   }
