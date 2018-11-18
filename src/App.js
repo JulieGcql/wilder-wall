5b6e2d942676a4fd29c454bb833af75a9e304c5a
Julie Gicquel <julie.gcql@gmail.com>
2018-11-18 13:11:39 +0100

    create resume

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import {Switch, Route} from 'react-router-dom'
 import './App.css';
 import NotFound from './components/NotFound';
 import JohnDoe from './components/wilders/JohnDoe/JohnDoe';
+import JulieGicquel from './components/wilders/JulieGicquel/JulieGicquel';
 
 class App extends Component {
   render() {
     return (
       <Switch>
         <Route exact path="/wilders/john-doe" component={JohnDoe} />
+        <Route exact path="/wilders/julie-gicquel" component={JulieGicquel} />
         <Route path="/" component={NotFound} />
       </Switch>
     );
