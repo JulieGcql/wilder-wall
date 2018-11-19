8f52edf7f3211468ea87d8d55fa0bef5dbdc281d
stephane job <darksh3ll.js@gmail.com>
2018-11-19 20:35:15 +0100

    mise a jour du profil

--- src/components/wilders/JobStephane/Header.js
+++ src/components/wilders/JobStephane/Header.js
@@ -7,13 +7,14 @@ import "./JobStephane.css"
 class Header extends Component {
     render () {
         return (
-           <header>
+           <header className="header_wild">
                 <h1 className="p30">{`Bonjour, je m'appelle ${this.props.firstname}`} </h1>
-                <h1 className="p30">Développeur FullStack junior: ReactJS , Redux, 🔥 NodeJS (express)</h1>
-                <h4 className="p30"><a href={this.props.linkedin}>Linkedin</a></h4>
+                <h1 className="p30">Développeur FullStack Junior: ReactJS , Redux, 🔥 NodeJS </h1>
+                <h4 className="p30">
+                    <a href={this.props.linkedin}>Linkedin</a></h4>
                 <h4 className="p30"><a href={this.props.github}>Github</a></h4>
                 <h4 className="p30">07.71.58.27.43</h4>
-               <a href="https://drive.google.com/open?id=1HXEfZsR0HRJ6IwyzkVW11rwm2UiiS9_l"><Button className="p30" color="primary">Télécharger mon cv</Button></a>
+               <a id ="photos_profile" href="https://drive.google.com/open?id=1HXEfZsR0HRJ6IwyzkVW11rwm2UiiS9_l"><Button className="p30 ombre_bleu" color="primary">Télécharger mon cv</Button></a>
 
            </header>
         )
