f6da3d4d32cc28df99004717ff7eaf8780f97018
David <eth3rnit3@outlook.fr>
2018-11-21 12:08:48 +0100

    change wall

--- src/components/wall/elements/Footer.jsx
+++ src/components/wall/elements/Footer.jsx
@@ -1,10 +1,34 @@
 import React, { Component } from 'react'
+import { Icon } from 'semantic-ui-react'
 import "../assets/css/footer.scss"
 export default class Footer extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      contactPhone: '06 31 00 45 71',
+      contactEmail: 'caroline.larcie@wildcodeschool.fr',
+      contactAddress: '1, place Francis Chirat, 13002 Marseille'
+    }
+  }
   render() {
     return (
-      <div className="wildersWallFooter" >
-        <p className="wilderCopyright">Wilders Wall - © Wild Code School {(new Date()).getFullYear()}</p>
+      <div>
+        <div className="wildersWallFooter" >
+          <a href={`tel:${this.state.contactPhone}`}>
+            <p>
+              <Icon name='phone' size='large'/>
+              {this.state.contactPhone}
+            </p>
+          </a>
+          <p className="wilderCopyright">Wilders Wall - © Wild Code School {(new Date()).getFullYear()}</p>
+          <a href={`mailto:${this.state.contactEmail}`}>
+            <p>
+              <Icon name='mail' size='large'/>
+              {this.state.contactEmail}
+            </p>
+          </a>
+        </div>
+        <p className="">Wilders Wall - © Wild Code School {(new Date()).getFullYear()}</p>
       </div>
     )
   }
